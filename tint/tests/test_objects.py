52686a50c74a4ae2fa5c7e7d967efcf136391c06
Mark Picel <mhpicel@gmail.com>
2018-05-17 14:38:26 -0500

    TST: Using numpy.testing instead of math

--- tint/tests/test_objects.py
+++ tint/tests/test_objects.py
@@ -1,7 +1,7 @@
 """ Unit tests for objects module. """
 
-from math import isclose
 import numpy as np
+from numpy.testing import assert_almost_equal, assert_allclose
 
 import pyart
 
@@ -21,19 +21,18 @@ grid_size = record.grid_size
 raw, filtered = extract_grid_data(grid, field, grid_size, params)
 
 
-
 def test_get_object_prop():
     """ Test calculation of object properties from grid file. """
     props = objects.get_object_prop(filtered, grid, field, record, params)
     obj1_props = dict([(key, val[0]) for key, val in props.items()])
-    assert(obj1_props['area'] == 8.5)
-    assert(np.all(obj1_props['center'] == np.array([160., 243.])))
-    assert(isclose(obj1_props['field_max'], 42.666893))
-    assert(isclose(obj1_props['grid_x'], 243.1759999999999))
-    assert(isclose(obj1_props['grid_y'], 160.2940000000))
-    assert(obj1_props['id1'] == 1)
-    assert(obj1_props['isolated'])
-    assert(isclose(obj1_props['lat'], 28.387))
-    assert(isclose(obj1_props['lon'], -93.836799999999))
-    assert(obj1_props['max_height'] == 4.5)
-    assert(obj1_props['volume'] == 15.5)
+    assert obj1_props['area'] == 8.5
+    assert_allclose(obj1_props['center'], np.array([160., 243.]), rtol=0.0001)
+    assert_almost_equal(obj1_props['field_max'], 42.6668, 3)
+    assert_almost_equal(obj1_props['grid_x'], 243.1759, 3)
+    assert_almost_equal(obj1_props['grid_y'], 160.2940, 3)
+    assert obj1_props['id1'] == 1
+    assert obj1_props['isolated']
+    assert_almost_equal(obj1_props['lat'], 28.387, 2)
+    assert_almost_equal(obj1_props['lon'], -93.836, 2)
+    assert obj1_props['max_height'] == 4.5
+    assert obj1_props['volume'] == 15.5
